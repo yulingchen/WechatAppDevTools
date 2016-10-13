3cdfbe57724dccd60c7730adef00d31c4bb89eb1
yulingchen <yulingchen@vip.qq.com>
2016-10-13 16:44:57 +0800

    format files

--- app/dist/components/develop/develop.js
+++ app/dist/components/develop/develop.js
@@ -1,12 +1,12 @@
 "use strict";
 
 function init() {
-    var e = require("../../lib/react.js"),
-        t = require("../simulator/controller.js"),
-        r = require("../debugger/debugger.js"),
-        s = require("../../cssStr/cssStr.js"),
-        o = require("../../stores/projectStores.js"),
-        i = e.createClass({
+    var React = require("../../lib/react.js"),
+        Simulator = require("../simulator/controller.js"),
+        Debugger = require("../debugger/debugger.js"),
+        cssStr = require("../../cssStr/cssStr.js"),
+        projectStores = require("../../stores/projectStores.js"),
+        Develop = React.createClass({
             displayName: "Develop",
             getInitialState: function() {
                 return { project: !!this.props.project }
@@ -15,16 +15,22 @@ function init() {
                 var e = this;
                 this.setState({ project: !1 }, function() { e.setState({ project: !0 }) })
             },
-            componentDidMount: function() { o.on("RESTART_PROJECT", this._restart) },
-            componentWillUnmount: function() { o.removeListener("RESTART_PROJECT", this._restart) },
+            componentDidMount: function() { projectStores.on("RESTART_PROJECT", this._restart) },
+            componentWillUnmount: function() { projectStores.removeListener("RESTART_PROJECT", this._restart) },
             render: function() {
                 var o = this.props.show,
-                    i = "debug" === o ? {} : Object.assign({}, s.webviewDisplayNone),
+                    i = "debug" === o ? {} : Object.assign({}, cssStr.webviewDisplayNone),
                     p = void 0;
-                return this.props.project ? this.state.project ? (p = e.createElement(t, { project: this.props.project }), i.marginTop = -52) : p = "" : p = e.createElement(t, { project: this.props.project }), e.createElement("div", { className: "develop", style: i }, p, e.createElement(r, { project: this.props.project, propshow: o }))
+                return this.props.project ? 
+                this.state.project ? (p = React.createElement(Simulator, { project: this.props.project }), i.marginTop = -52) : p = "" : 
+                p = React.createElement(Simulator, { project: this.props.project }), 
+                React.createElement("div", { className: "develop", style: i }, 
+                	p, 
+                	React.createElement(Debugger, { project: this.props.project, propshow: o })
+                	)
             }
         });
-    _exports = i
+    _exports = Develop
 }
 var _exports;
 init(), module.exports = _exports;
